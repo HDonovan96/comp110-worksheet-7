f2251e10ed80a177d888657f8567fa271b6cb4fb
HDonovan96 <harry.bd11@gmail.com>
2019-11-27 14:41:20 +0000

    In 'DirectoryUtils' added comments

--- comp110-worksheet-7/DirectoryUtils.cs
+++ comp110-worksheet-7/DirectoryUtils.cs
@@ -24,8 +24,10 @@ namespace comp110_worksheet_7
 		// Return the total size, in bytes, of all the files below the given directory
 		public static long GetTotalSize(string directory)
 		{
+			// Gets every file in 'directory'.
             string[] filesInDirectory = Directory.GetFiles(directory);
 			long totalSize = 0;
+			// Adds the size of each file to 'totalSize'
             foreach (string element in filesInDirectory)
             {
 				totalSize += GetFileSize(element);
@@ -37,26 +39,27 @@ namespace comp110_worksheet_7
 		// Return the number of files (not counting directories) below the given directory
 		public static int CountFiles(string directory)
 		{
+			// Gets every file in 'directory'.
 			string[] filesInDirectory = Directory.GetFiles(directory);
-			int count = 0;
-			foreach (string element in filesInDirectory)
-			{
-				count++;				
-			}
 
-			return count++;
+			// Returns the number of elements in 'filesInDirectory'.
+			return filesInDirectory.Length;
 		}
 
 		// Return the nesting depth of the given directory. A directory containing only files (no subdirectories) has a depth of 0.
 		public static int GetDepth(string directory)
 		{
+			// Gets every sub-directory in 'directory'.
 			string[] directoriesInDirectory = Directory.GetDirectories(directory);
 			int depth = 0;
+			// Is used to store the result from recursive calls of 'GetDepth'.
 			int elementDepth;
 
+			// If there are any sub-directories under 'directory' returns at least one.
 			foreach (string element in directoriesInDirectory)
 			{
 				elementDepth = GetDepth(element) + 1;
+				// 'depth' is only overwritten if the depth of the sub-directory just checked was greater than the deepest directory already checked.
 				if (elementDepth > depth)
 				{
 					depth = elementDepth;
@@ -69,6 +72,7 @@ namespace comp110_worksheet_7
 		// Get the path and size (in bytes) of the smallest file below the given directory
 		public static Tuple<string, long> GetSmallestFile(string directory)
 		{
+			// Gets every element in 'directory', this includes files or sub-directories.
 			string[] elementsInDirectory = Directory.GetFileSystemEntries(directory);
 			Tuple<string, long> smallestFile = new Tuple<string, long>(String.Empty, 0);
 			Tuple<string, long> smallestFileInSubDir;
@@ -76,9 +80,12 @@ namespace comp110_worksheet_7
 
 			foreach (string element in elementsInDirectory)
 			{
+				// If the element is a directory then 'GetSmallestFile' is called recursively.
+				// This means all sub-directories are also checked.
 				if (IsDirectory(element))
 				{
 					smallestFileInSubDir = GetSmallestFile(element);
+					// The ' smallestFile.Item2 == 0' is needed to stop this function always returning a value of zero. 
 					if (smallestFile.Item2 > smallestFileInSubDir.Item2 || smallestFile.Item2 == 0)
 					{
 						smallestFile = smallestFileInSubDir;
@@ -87,6 +94,7 @@ namespace comp110_worksheet_7
 				else
 				{
 					currentFileSize = GetFileSize(element);
+					// The ' smallestFile.Item2 == 0' is needed to stop this function always returning a value of zero. 
 					if (smallestFile.Item2 > currentFileSize || smallestFile.Item2 == 0)
 					{
 						smallestFile = new Tuple<string, long>(element, currentFileSize);
@@ -98,8 +106,10 @@ namespace comp110_worksheet_7
 		}
 
 		// Get the path and size (in bytes) of the largest file below the given directory
+		// This function is functionally similar to 'GetSmallestFile', checking for a larger file size rather than a smaller one.
 		public static Tuple<string, long> GetLargestFile(string directory)
 		{
+			// Gets every element in 'directory', this includes files or sub-directories.
 			string[] elementsInDirectory = Directory.GetFileSystemEntries(directory);
 			Tuple<string, long> largestFile = new Tuple<string, long>(String.Empty, 0);
 			Tuple<string, long> largestFileInSubDir;
@@ -107,10 +117,12 @@ namespace comp110_worksheet_7
 
 			foreach (string element in elementsInDirectory)
 			{
+				// If the element is a directory then 'GetLargestFile' is called recursively.
+				// This means all sub-directories are also checked.
 				if (IsDirectory(element))
 				{
 					largestFileInSubDir = GetLargestFile(element);
-					if (largestFile.Item2 < largestFileInSubDir.Item2 || largestFile.Item2 == 0)
+					if (largestFile.Item2 < largestFileInSubDir.Item2)
 					{
 						largestFile = largestFileInSubDir;
 					}
@@ -118,7 +130,7 @@ namespace comp110_worksheet_7
 				else
 				{
 					currentFileSize = GetFileSize(element);
-					if (largestFile.Item2 < currentFileSize || largestFile.Item2 == 0)
+					if (largestFile.Item2 < currentFileSize)
 					{
 						largestFile = new Tuple<string, long>(element, currentFileSize);
 					}
@@ -131,6 +143,7 @@ namespace comp110_worksheet_7
 		// Get all files whose size is equal to the given value (in bytes) below the given directory
 		public static List<string> GetFilesOfSize(string directory, long size)
 		{
+			// Gets every element in 'directory', this includes files or sub-directories.
 			string[] elementsInDirectory = Directory.GetFileSystemEntries(directory);
 			List<string> allFilesOfSize = new List<string>();
 
@@ -138,6 +151,8 @@ namespace comp110_worksheet_7
 			{
 				if (IsDirectory(element))
 				{
+					// If the element is a directory then 'GetFilesOfSize' is called recursively.
+					// This means all sub-directories are also checked.
 					List<string> filesInSub = GetFilesOfSize(element, size);
 					foreach (string file in filesInSub)
 					{
